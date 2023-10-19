d92b3ea2323e16d8a3017fb55be896dffb4e292b
Kenneth Rosas <kennethrosas.gg@gmail.com>
2023-10-19 17:38:12 +0800

    Integrate Firebase Auth
    Basic Firebase Authentication Using Email & Password

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:lightbulb/screens/LoginPage.dart';
+import 'package:lightbulb/screens/CheckAuthScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      ///When the application start check first if user already signin
+      home: CheckAuth(),
     );
   }
 }
---
+++ lib/utils/AuthService.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:lightbulb/screens/HomeScreen.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Future<void> login(
+      {required BuildContext context,
+      required String username,
+      required String password}) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: username,
+        password: password,
+      );
+      User user = userCredential.user!;
+      print('User signed in: ${user.uid}');
+      // Navigate to the home screen after successful login
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } catch (e) {
+      print('Error signing in: $e');
+    }
+  }
+}
